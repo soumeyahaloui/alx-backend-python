0f29534f7856a347fc4c8eb58178eddc7c55c8a3
soumeyahaloui <soumeyahaloui@gmail.com>
2024-01-11 15:55:21 -0800

    updated

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python3
-"""
-This module provides a function to concatenate two strings.
-"""
+'''Task 1's module.
+'''
+
 
 def concat(str1: str, str2: str) -> str:
-    """
-    Concatenate two strings and return the result.
-
-    Args:
-    str1 (str): The first string.
-    str2 (str): The second string.
-
-    Returns:
-    str: The concatenated string.
-    """
+    '''Concatenates two strings.
+    '''
     return str1 + str2
-
--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,26 +1,9 @@
 #!/usr/bin/env python3
-"""
-This module provides a function 'floor' that computes the floor of a float.
-"""
+'''Task 2's module.
+'''
 
-def floor(n: float) -> int:
-    """
-    Return the floor of the float n.
 
-    Args:
-    n (float): The float number to compute the floor of.
-
-    Returns:
-    int: The floor of the float.
-    """
-    return int(n // 1)
-
-if __name__ == "__main__":
-    import math
-
-    # Testing the function
-    ans = floor(3.14)
-    print(ans == math.floor(3.14))
-    print(floor.__annotations__)
-    print(f"floor(3.14) returns {ans}, which is a {type(ans)}")
-
+def floor(a: float) -> int:
+    '''Computes the floor of  floating-point number.
+    '''
+    return int(a)
